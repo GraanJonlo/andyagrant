e4f43a8148efa15e54819efcef41368c7348333e
Andy Grant <andy.a.grant@gmail.com>
2018-02-04 09:07:17 +0000

    Experiments in lazy loading

--- src/SpaUi/ClientApp/components/Home.tsx
+++ src/SpaUi/ClientApp/components/Home.tsx
@@ -6,17 +6,33 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div className='container'>
                     <div className='columns'>
-                        <div className='column is-two-thirds'>
+                        <div className='column is-half'>
                             <div className='card'>
                                 <Link to={'/videopost/1234'}>
                                     <div className="card-image">
-                                        <figure className="image is-4by3">
-                                            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                        <figure className="image is-16by9">
+                                        <img src="/images/xpman616.svg" className='imgPlaceholder' data-large='/images/xpman616.jpg' alt='Placeholder image' />
                                         </figure>
                                     </div>
                                 </Link>
                                 <div className='card-content'>
-                                    <h2 className='title'>XPMan 61.5 - Abstractions</h2>
+                                    <h2 className='title is-4'>XPMan 61.6 - Bug Free Code</h2>
+                                    <p>Gemma Cameron at the Extreme Programming Manchester 2015 Lightning Talks.</p>
+                                    <p><time dateTime="2018-02-02">2/2/2018</time></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='column'>
+                            <div className='card'>
+                                <Link to={'/videopost/1234'}>
+                                    <div className="card-image">
+                                        <figure className="image is-16by9">
+                                        <img src="/images/xpman615.svg" className='imgPlaceholder' data-large='/images/xpman615.jpg' alt='Placeholder image' />
+                                        </figure>
+                                    </div>
+                                </Link>
+                                <div className='card-content'>
+                                    <h2 className='title is-4'>XPMan 61.5 - Abstractions</h2>
                                     <p>Matt Cannon at the Extreme Programming Manchester 2015 Lightning Talks.</p>
                                     <p><time dateTime="2018-02-02">2/2/2018</time></p>
                                 </div>
@@ -26,13 +42,13 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                             <div className='card'>
                                 <Link to={'/videopost/1234'}>
                                     <div className="card-image">
-                                        <figure className="image is-4by3">
-                                            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                        <figure className="image is-16by9">
+                                            <img src="/images/xpman614.svg" className='imgPlaceholder' data-large='/images/xpman614.jpg' alt='Placeholder image' />
                                         </figure>
                                     </div>
                                 </Link>
                                 <div className='card-content'>
-                                    <h2 className='title'>XPMan 61.4 - Collaboration</h2>
+                                    <h2 className='title is-4'>XPMan 61.4 - Collaboration</h2>
                                     <p>Clare Sudbery at the Extreme Programming Manchester 2015 Lightning Talks.</p>
                                     <p><time dateTime="2018-02-02">2/2/2018</time></p>
                                 </div>
@@ -44,13 +60,13 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                             <div className='card'>
                                 <Link to={'/videopost/1234'}>
                                     <div className="card-image">
-                                        <figure className="image is-4by3">
-                                            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                        <figure className="image is-16by9">
+                                            <img src="/images/xpman613.svg" className='imgPlaceholder' data-large='/images/xpman613.jpg' alt='Placeholder image' />
                                         </figure>
                                     </div>
                                 </Link>
                                 <div className='card-content'>
-                                    <h2 className='title'>XPMan 61.3 - Hidden Queues</h2>
+                                    <h2 className='title is-4'>XPMan 61.3 - Hidden Queues</h2>
                                     <p>James Jeffries at the Extreme Programming Manchester 2015 Lightning Talks.</p>
                                     <p><time dateTime="2018-02-02">2/2/2018</time></p>
                                 </div>
@@ -60,13 +76,13 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                             <div className='card'>
                                 <Link to={'/videopost/1234'}>
                                     <div className="card-image">
-                                        <figure className="image is-4by3">
-                                            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                        <figure className="image is-16by9">
+                                            <img src="/images/xpman612.svg" className='imgPlaceholder' data-large='/images/xpman612.jpg' alt='Placeholder image' />
                                         </figure>
                                     </div>
                                 </Link>
                                 <div className='card-content'>
-                                    <h2 className='title'>XPMan 61.2 - Microservices</h2>
+                                    <h2 className='title is-4'>XPMan 61.2 - Microservices</h2>
                                     <p>Nima Montazeri at the Extreme Programming Manchester 2015 Lightning Talks.</p>
                                     <p><time dateTime="2018-02-02">2/2/2018</time></p>
                                 </div>
@@ -74,21 +90,22 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                         </div>
                         <div className='column'>
                             <div className='card'>
-                            <Link to={'/videopost/1234'}>
-                                <div className="card-image">
-                                    <figure className="image is-16by9">
-                                        <img src="/images/xpman611.jpg" alt="Placeholder image" />
-                                    </figure>
+                                <Link to={'/videopost/1234'}>
+                                    <div className="card-image">
+                                        <figure className="image is-16by9">
+                                            <img src="/images/xpman611.svg" className='imgPlaceholder' data-large='/images/xpman611.jpg' alt='Placeholder image' />
+                                        </figure>
+                                    </div>
+                                </Link>
+                                <div className='card-content'>
+                                    <h2 className='title is-4'>XPMan 61.1 - We Said It Would Be Simple, Not Easy</h2>
+                                    <p>Andy Longshaw at the Extreme Programming Manchester 2015 Lightning Talks.</p>
+                                    <p><time dateTime="2018-02-02">2/2/2018</time></p>
                                 </div>
-                            </Link>
-                            <div className='card-content'>
-                                <h2 className='title is-4'>XPMan 61.1 - We Said It Would Be Simple, Not Easy</h2>
-                                <p>Andy Longshaw at the Extreme Programming Manchester 2015 Lightning Talks.</p>
-                                <p><time dateTime="2018-02-02">2/2/2018</time></p>
                             </div>
                         </div>
+                        <div className='column'></div>
                     </div>
-                </div>
-            </div>;
+                </div>;
     }
 }
