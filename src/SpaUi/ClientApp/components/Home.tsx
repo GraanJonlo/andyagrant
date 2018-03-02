7e97b9f175497d3b8ba11460b365e3b7ee720be1
Andy Grant <andy.a.grant@gmail.com>
2018-03-02 11:32:48 +0000

    Trying Cloudfront

--- src/SpaUi/ClientApp/components/Home.tsx
+++ src/SpaUi/ClientApp/components/Home.tsx
@@ -13,7 +13,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                                 id='xpman62'
                                 title='XPMan 62 - In Defence of Mocks, with Mark Kirschstein'
                                 summary='<p>Are mocks pointless? This session argues maybe, maybe not…</p>'
-                                image='/images/xpman62.jpg'
+                                image='https://d20sprsywyfv7z.cloudfront.net/images/xpman62.jpg'
                                 timestamp='2018-02-04' />
                         </div>
                         <div className='column'>
@@ -21,7 +21,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                                 id='xpman616'
                                 title='XPMan 61.6 - Bug Free Code'
                                 summary='<p>Gemma Cameron at the Extreme Programming Manchester 2015 Lightning Talks.</p>'
-                                image='/images/xpman616.jpg'
+                                image='https://d20sprsywyfv7z.cloudfront.net/images/xpman616.jpg'
                                 timestamp='2018-02-04' />
                         </div>
                         <div className='column'>
@@ -29,7 +29,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                                 id='xpman615'
                                 title='XPMan 61.5 - Abstractions'
                                 summary='<p>Matt Cannon at the Extreme Programming Manchester 2015 Lightning Talks.</p>'
-                                image='/images/xpman615.jpg'
+                                image='https://d20sprsywyfv7z.cloudfront.net/images/xpman615.jpg'
                                 timestamp='2018-02-04' />
                         </div>
                     </div>
@@ -39,7 +39,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                                 id='xpman614'
                                 title='XPMan 61.4 - Collaboration'
                                 summary='<p>Clare Sudbery at the Extreme Programming Manchester 2015 Lightning Talks.</p>'
-                                image='/images/xpman614.jpg'
+                                image='https://d20sprsywyfv7z.cloudfront.net/images/xpman614.jpg'
                                 timestamp='2018-02-04' />
                         </div>
                         <div className='column'>
@@ -47,7 +47,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                                 id='xpman613'
                                 title='XPMan 61.3 - Hidden Queues'
                                 summary='<p>James Jeffries at the Extreme Programming Manchester 2015 Lightning Talks.</p>'
-                                image='/images/xpman613.jpg'
+                                image='https://d20sprsywyfv7z.cloudfront.net/images/xpman613.jpg'
                                 timestamp='2018-02-04' />
                         </div>
                         <div className='column'>
@@ -55,7 +55,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                                 id='xpman612'
                                 title='XPMan 61.2 - Microservices'
                                 summary='<p>Nima Montazeri at the Extreme Programming Manchester 2015 Lightning Talks.</p>'
-                                image='/images/xpman612.jpg'
+                                image='https://d20sprsywyfv7z.cloudfront.net/images/xpman612.jpg'
                                 timestamp='2018-02-04' />
                         </div>
                         <div className='column'>
@@ -63,7 +63,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                                 id='xpman611'
                                 title='XPMan 61.1 - We Said It Would Be Simple, Not Easy'
                                 summary='<p>Andy Longshaw at the Extreme Programming Manchester 2015 Lightning Talks.</p>'
-                                image='/images/xpman611.jpg'
+                                image='https://d20sprsywyfv7z.cloudfront.net/images/xpman611.jpg'
                                 timestamp='2018-02-04' />
                         </div>
                     </div>
